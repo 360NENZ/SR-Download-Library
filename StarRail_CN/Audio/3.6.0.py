a87abc5609cd7c3367c6d07aea88dd89b3640a91
Cyt <qinfyy233@gmail.com>
2025-09-22 21:39:59 +0800

    Add the download link for 3.6.0

---
+++ StarRail_CN/Audio/3.6.0.py
@@ -0,0 +1,21 @@
+**[Chinese | 3.6.0](https://autopatchcn.bhsr.com/client/cn/20250913163925_dgTMhCJ7OUmyjlsh/PC/Chinese.7z)**
+
+**[Update | 3.5.0 - 3.6.0](https://autopatchcn.bhsr.com/client/diff/hkrpg_cn/audio_zh-cn_3.5.0_3.6.0_hdiff_RdgUYTTQyzClDeTm.7z)**
+
+---
+
+**[English | 3.6.0](https://autopatchcn.bhsr.com/client/cn/20250913163925_dgTMhCJ7OUmyjlsh/PC/English.7z)**
+
+**[Update | 3.5.0 - 3.6.0](https://autopatchcn.bhsr.com/client/diff/hkrpg_cn/audio_en-us_3.5.0_3.6.0_hdiff_BEyyaZjWjSzQLfgh.7z)**
+
+---
+
+**[Japanese | 3.6.0](https://autopatchcn.bhsr.com/client/cn/20250913163925_dgTMhCJ7OUmyjlsh/PC/Japanese.7z)**
+
+**[Update | 3.5.0 - 3.6.0](https://autopatchcn.bhsr.com/client/diff/hkrpg_cn/audio_ja-jp_3.5.0_3.6.0_hdiff_yQVPNjdXxBfzjtUW.7z)**
+
+---
+
+**[Korean | 3.6.0](https://autopatchcn.bhsr.com/client/cn/20250913163925_dgTMhCJ7OUmyjlsh/PC/Korean.7z)**
+
+**[Update | 3.5.0 - 3.6.0](https://autopatchcn.bhsr.com/client/diff/hkrpg_cn/audio_ko-kr_3.5.0_3.6.0_hdiff_FyGKqnaQxpaudPuZ.7z)**
